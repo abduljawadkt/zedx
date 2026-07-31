c93b2611ce6f7ff88f30c430b021d620387ed598
abduljawadkt <iamjawad001@gmail.com>
2026-07-31 20:19:50 +0530

    Move featured items below essentials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,8 +30,8 @@ export default function Home() {
     <main className="flex-1 -mt-[5rem]">
       <Hero />
       <CategoryStrip />
-      <TrendingProducts />
       <ImmersiveProductStories />
+      <TrendingProducts />
       <StickyProductStory />
       <CategoryUniverse />
       <BrandExperience />
